34bcb89a30bbbbd06a8859021506b2ea9a7c3122
Mandy <Lmandy239@gmail.com>
2023-12-01 18:49:21 -0500

    CargoCarrier % CombatReady SubClasess made

---
+++ src/com/skilldistillery/jets/CargoCarrier.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.jets;
+
+public class CargoCarrier extends CargoPlane {
+
+	@Override
+	public void loadCargo() {
+		super.loadCargo();
+	}
+
+	
+}
---
+++ src/com/skilldistillery/jets/CombatReady.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.jets;
+
+public class CombatReady extends fighterJet {
+
+	@Override
+	public void fight() {
+	
+		super.fight();
+	}
+
+}
