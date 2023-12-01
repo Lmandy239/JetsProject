dd666678aabe0ceca68afcf9669a2fcbf1d33bcc
Mandy <Lmandy239@gmail.com>
2023-12-01 18:18:36 -0500

    Intitial menu and run methods in app

---
+++ src/com/skilldistillery/jets/Fly.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.jets;
+
+public class Fly {
+
+}
--- src/com/skilldistillery/jets/JetsApplication.java
+++ src/com/skilldistillery/jets/JetsApplication.java
@@ -1,9 +1,32 @@
 package com.skilldistillery.jets;
 
 public class JetsApplication {
+	
 	private AirField airField;
+	
 	public static void main(String[] args) {
-
+		JetsApplication shipApp = new JetsApplication();
+		System.out.println("Hello, welcome to our Ships application. What would you like to do?: ");
+		shipApp.run();
+		
+		
+	}
+	
+	public void run() {
+		menu();
+	}
+	
+	
+	public void menu() {
+		System.out.println("1. List fleet\n"
+				+ "2. Fly all ships\n"
+				+ "3. View fastest ship\n"
+				+ "4. View ship with longest range\n"
+				+ "5. Load all Cargo ships\n"
+				+ "6. Dogfight!\n"
+				+ "7. Add a ship to Fleet\n"
+				+ "8. Remove a ship from Fleet\n"
+				+ "9. Quit");
 	}
 
 }
